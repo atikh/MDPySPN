78cf42a2d6c57dec028139ceb1702127b0e30090
Atieh Khodadadi <46083261+atikh@users.noreply.github.com>
2025-01-13 11:17:21 +0100

    Update Main.py

--- examples/Main.py
+++ examples/Main.py
@@ -6,10 +6,10 @@ sys.path.append('MDSPN') # please provide the path
 #########################
 ## Imports ##
 #########################
-from modules.spn import *
-from modules.spn_simulate import simulate
-from modules.spn_visualization import *
-from modules.spn import SPN
+from components.spn import *
+from components.spn_simulate import simulate
+from components.spn_visualization import *
+from components.spn import SPN
 
 ## Define Dimensions and return to all ##
 #########################
