77e30aa69bd1ec481db877d3cb1a6418668763a0
Atieh Khodadadi <46083261+atikh@users.noreply.github.com>
2025-01-13 12:07:47 +0100

    Update Main.py

--- examples/Main.py
+++ examples/Main.py
@@ -1,7 +1,8 @@
 import sys
 ##sys.path.append('../MDPySPN')
+import sys
+import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
-
 
 #########################
 ## Imports ##
